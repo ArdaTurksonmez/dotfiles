67214ca8fe6bf12c3860d50511a9565516426a1d
Arda <125517989+ArdaTurksonmez@users.noreply.github.com>
2023-08-12 11:55:05 -0400

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,11 +6,11 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono:size=10" };
-static const char col_gray1[]       = "#222222"; // background color
-static const char col_gray2[]       = "#282828"; // inactive window border color
-static const char col_gray3[]       = "#ebdbb2"; // font color
-static const char col_gray4[]       = "#eeeeee"; // current tag and current window font color
-static const char col_cyan[]        = "#d65d0e"; // top bar second color and active window border color
+static const char col_gray1[]       = "#000000"; // background color
+static const char col_gray2[]       = "#000000"; // inactive window border color
+static const char col_gray3[]       = "#FFFFFF"; // font color
+static const char col_gray4[]       = "#FFFFFF"; // current tag and current window font color
+static const char col_cyan[]        = "#000000"; // top bar second color and active window border color
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
