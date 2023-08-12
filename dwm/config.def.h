c9f7b5a82b620410b0ec72d63bd28546b35302bc
Arda <125517989+ArdaTurksonmez@users.noreply.github.com>
2023-08-12 14:29:43 -0400

    update

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -9,8 +9,8 @@ static const char *fonts[]          = { "JetBrainsMono:size=10" };
 static const char col_gray1[]       = "#000000"; // background color
 static const char col_gray2[]       = "#000000"; // inactive window border color
 static const char col_gray3[]       = "#FFFFFF"; // font color
-static const char col_gray4[]       = "#000000"; // current tag and current window font color
-static const char col_cyan[]        = "#FFFFFF"; // top bar second color and active window border color
+static const char col_gray4[]       = "#646464"; // current tag and current window font color
+static const char col_cyan[]        = "#000000"; // top bar second color and active window border color
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
